351aa823f2cbc9c8a8a1289749ff7dbc85808363
Rudson Souza <rudsonsouza@Rudsons-MacBook-Air.local>
2022-08-28 21:21:58 -0300

    Longest Peak

--- Arrays/Medium/spiralTraverse.py
+++ Arrays/Medium/spiralTraverse.py
@@ -1,5 +1,6 @@
+# time O(n) | space O(n)
 class Solution(object):
-    def spiralTraverse(array):
+    def spiralTraverseIterative(array):
         result = []
         startRow, endRow = 0, len(array) - 1
         startCol, endCol = 0, len(array[0]) - 1
@@ -28,6 +29,35 @@ class Solution(object):
             startCol += 1
             endRow -= 1
         return result
+    
+    def spiralTraverseRecursive(array):
+        result = []
+        spiralFill(array, 0, len(array) - 1, 0, len(array[0]) - 1, result)
+        return result
+
+def spiralFill(array, startRow, endRow, startCol, endCol, result):
+    if startRow > endRow or startCol > endCol:
+        return
+    
+    for col in range(startCol, endCol + 1):
+        result.append(array[startCol][col])
+
+    for row in range(startRow + 1, endRow + 1):
+        result.append(array[row][endCol])
+
+    for col in reversed(range(startCol, endCol)):
+        if startRow == endRow:
+            break
+
+        result.append(array[endRow][col])
+
+    for row in reversed(range(startRow + 1, endRow)):
+        if startCol == endCol:
+            break
+
+        result.append(array[row][startCol])
+
+    spiralFill(array, startRow + 1, endRow - 1, startCol + 1, endCol - 1, result)
 
 array = [
     [1 , 2 , 3 , 4],
@@ -35,4 +65,4 @@ array = [
     [11, 16, 15, 6],
     [10,  9,  8, 7]
 ]
-print(Solution.spiralTraverse(array))
+print(Solution.spiralTraverseRecursive(array))
