70da2b482971a8b1bb91eee6d445ef70053f4486
rudsonsouza <rudson-souza@hotmail.com>
2024-02-24 17:20:30 -0300

    Transpose Matrix question

---
+++ python/Array/transposeMatrix/transposeMatrix.py
@@ -0,0 +1,12 @@
+def transposeMatrix(matrix):
+    transposedMatrix = []
+    for col in range(len(matrix[0])):
+        newRow = []
+        for row in range(len(matrix)):
+            newRow.append(matrix[row][col])
+        transposedMatrix.append(newRow)
+    return transposedMatrix
+
+
+matrixRequest = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+transposeMatrix(matrixRequest)
