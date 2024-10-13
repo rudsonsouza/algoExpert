a35f21d39b1566e8322562fecc29eeaae8e1b05f
rudsonsouza <rudson-souza@hotmail.com>
2024-10-13 18:21:59 -0300

    FindLoop - LinkedList | RiverSize - Graph

---
+++ c#/algoExpert/FindLoop/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+/* Find Loop
+ *
+ * Write a function that takes in the head of a Singly Linked List that contains a loop (in other words, the
+ * list's tail node points to some node in the list instead of None/null). The function should return
+ * the node (the actual node--not just its value) from which the loop originates in constant space.
+ *
+ * Each LinkedList node has an integer value as well as a next node pointing to the next node
+ * in the list.
+ *
+ * Sample Input
+ * head = 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6
+ *                                      v
+ *                            9 <- 8 <- 7
+ */
+
+Console.WriteLine("Hello, World!");
+
+static LinkedList FindLoop(LinkedList head)
+{
+    LinkedList first = head.next;
+    LinkedList second = head.next.next;
+
+    while (first != second)
+    {
+        first = first.next;
+        second = second.next.next;
+    }
+
+    first = head;
+    while (first != second)
+    {
+        first = first.next;
+        second = second.next;
+    }
+
+    return first;
+}
+
+public class LinkedList
+{
+    public int value;
+    public LinkedList next = null;
+
+    public LinkedList(int value)
+    {
+        this.value = value;
+    }
+}
---
+++ c#/algoExpert/RiverSizes/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ * River Sizes
+ *
+ * You're given a two-dimensional array(a matrix) of potentially unequal height and width containing
+ * only 0s and 1s. Each 0 represents land, and each 1 represents part of a river. A river
+ * consists of any number of 1s that are either horizontally or vertically adjacent (but not diagonally
+ * adjacent). The number of adjacent 1s forming a river determine its size.
+ *
+ * Note that a river can twist. In other words, it doesn't have to be a straight vertical line or a straight
+ * horizontal line; it can be L-shaped, for example.
+ *
+ * Write a function that returns an array of the sizes of all rivers represented in the input matrix. The
+ * sizes don't need to be in any particular order.
+ */
+
+Console.WriteLine("Hello, World!");
+
+int[,] input =
+{
+    { 1, 0, 0, 1, 0 },
+    { 1, 0, 1, 0, 0 },
+    { 0, 0, 1, 0, 1 },
+    { 1, 0, 1, 0, 1 },
+    { 1, 0, 1, 1, 0 }
+};
+
+var result = RiverSizes(input);
+
+// O(wh) time | O(wh) space
+static List<int> RiverSizes(int[,] matrix)
+{
+    List<int> sizes = new List<int>();
+    bool[,] visited = new bool[matrix.GetLength(0), matrix.GetLength(1)];
+    
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (visited[i, j] == true) continue;
+
+            traverseNode(i, j, matrix, visited, sizes);
+        }
+    }
+
+    return sizes;
+}
+
+static void traverseNode(int i, int j, int[,] matrix, bool[,] visited, List<int> sizes)
+{
+    
+}
