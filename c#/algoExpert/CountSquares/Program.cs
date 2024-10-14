15f7a5c3def82721207592fe96f7a043368da53b
rudsonsouza <rudson-souza@hotmail.com>
2024-10-14 11:38:45 -0300

    Count Squares problem

---
+++ c#/algoExpert/CountSquares/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ * Count Squares
+ *
+ * Write a function that takes in a list of Cartesian coordinates(i.e., (x,y) coordinates) and returns the number of squares
+ * that can be formed by these coordinates.
+ *
+ * A square must have its four corners amongst the coordinates in order to be counted. A single coordinate can be used as a
+ * corner for multiple different squares.
+ *
+ * You can also assume that no coordinate will be farther than 100 units from the origin.
+ */
+
+Console.WriteLine("Hello, World!");
+
+var points = new int[][] {
+ new int[] { 1, 1 },
+ new int[] { 0, 0 },
+ new int[] { 0, 1 },
+ new int[] { 1, 0 }
+};
+
+CountSquares(points);
+
+int CountSquares(int[][] points)
+{
+   HashSet<string> pointsSet = new HashSet<string>();
+   foreach (var point in points)
+   {
+       pointsSet.Add(pointToString(point));
+   }
+
+   int count = 0;
+   foreach (var pointA in points)
+   { 
+    foreach (var pointB in points)
+    {
+      if (pointA == pointB) continue;
+     
+      double[] midPoint = new double[] { (pointA[0] + pointB[0]) / 2.0 , (pointA[1] + pointB[1]) / 2.0 };
+      double xDistanceFromMid = pointA[0] - midPoint[0];
+      double yDistanceFromMid = pointA[1] - midPoint[1];
+
+      double[] pointC = new double[] { midPoint[0] + yDistanceFromMid, midPoint[1] - xDistanceFromMid };
+      double[] pointD = new double[] { midPoint[0] - yDistanceFromMid, midPoint[1] + xDistanceFromMid };
+
+      if (pointsSet.Contains(dbPointToString(pointC)) && pointsSet.Contains(dbPointToString(pointD))) count++;
+    }
+   }
+
+   return count / 4;
+}
+
+string pointToString(int[] point)
+{
+ return point[0] + "," + point[1];
+}
+
+string dbPointToString(double[] point)
+{
+ if (point[0] % 1 == 0 && point[1] % 1 == 0)
+  return (int)point[0] + "," + (int)point[1];
+ 
+ return point[0] + "," + point[1];
+}
---
+++ c#/algoExpert/Quickselect/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Hello, World!");
+
+Quickselect(new int[] { 8, 5, 2, 9, 7, 6, 3 }, 3);
+
+static int Quickselect(int[] array, int k)
+{
+    int position = k - 1;
+    return QuickselectMethod(array, 0, array.Length - 1 , position);
+}
+
+static int QuickselectMethod(int[] array, int startIdx, int endIdx, int position)
+{
+    while (true)
+    {
+        if (startIdx > endIdx) return -1;
+
+        int pivotIdx = startIdx;
+        int leftIdx = startIdx + 1;
+        int rightIdx = endIdx;
+
+        while (leftIdx <= rightIdx)
+        {
+            if (array[leftIdx] > array[pivotIdx] && array[rightIdx] < array[pivotIdx])
+            {
+                swap(leftIdx, rightIdx, array);
+            }
+
+            if (array[leftIdx] <= array[pivotIdx]) leftIdx++;
+
+            if (array[rightIdx] >= array[pivotIdx]) rightIdx--;
+        }
+
+        swap(pivotIdx, rightIdx, array);
+        if (rightIdx == pivotIdx)
+        {
+            return array[rightIdx];
+        }
+        else if (rightIdx < position)
+        {
+            startIdx = rightIdx + 1;
+        }
+        else
+        {
+            endIdx = rightIdx - 1;
+        }
+    }
+}
+
+static void swap(int i, int j, int[] array)
+{
+    int temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
+}
