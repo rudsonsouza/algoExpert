3cebc3a9c4b8a6f135c269bf2ce3e1567fcada96
rudsonsouza <rudson-souza@hotmail.com>
2024-11-01 18:17:31 -0300

    Min number of jumps

---
+++ c#/algoExpert/LongestCommonSubsequence/Program.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Longest Common Subsequence
+ *
+ * Write a function that takes in two strings and returns their longest common
+ * subsequence.
+ *
+ * A subsequence of a string is a set of characters that aren't necessarily adjacent in
+ * the string but that are in the same order as they appear in the string. For instance,
+ * the characters ["a", "c", "d"] form a subsequence of the string "abcd",
+ * and so do the characters ["b", "d"]. Note that a single character in a string
+ * and the string itself are both valid subsequences of the string.
+ *
+ * You can assume that there will only be one longest common subsequence.
+ * 
+ */
+
+var str1 = "ZXVVYZW";
+var str2 = "XKYKZPW";
+
+var result = LongestCommonSubsequence(str1, str2);
+
+Console.WriteLine(result);
+
+static List<char> LongestCommonSubsequence(string str1, string str2)
+{
+    int[,][] lcs = new int[str2.Length + 1, str1.Length + 1][];
+    for (int i = 0; i < str2.Length + 1; i++)
+    {
+        for (int j = 0; j < str1.Length + 1; j++)
+        {
+            lcs[i, j] = new int[] { 0, 0, 0, 0 };
+        }
+    }
+
+    for (int i = 1; i < str2.Length + 1; i++)
+    {
+        for (int j = 1; j < str1.Length + 1; j++)
+        {
+            if (str2[i - 1] == str1[j - 1])
+            {
+                int[] newEntry = { (int)str2[i - 1], lcs[i - 1, j - 1][1] + 1, i - 1, j - 1 };
+                lcs[i, j] = newEntry;
+            } else if (lcs[i - 1, j][1] > lcs[i, j - 1][1])
+            {
+                int[] newEntry = { -1, lcs[i - 1, j][1], i - 1, j };
+                lcs[i, j] = newEntry;
+            }
+            else
+            {
+                int[] newEntry = { -1, lcs[i, j - 1][1], i, j - 1 };
+                lcs[i, j] = newEntry;
+            }
+        }
+    }
+    
+    return buildSequence(lcs);
+}
+
+static List<char> buildSequence(int[,][] lcs)
+{
+    List<char> sequence = new List<char>();
+    int i = lcs.GetLength(0) - 1;
+    int j = lcs.GetLength(1) - 1;
+    while (i != 0 && j != 0)
+    {
+        int[] currentEntry = lcs[i, j];
+        if (currentEntry[0] != -1)
+        {
+            sequence.Insert(0, (char)currentEntry[0]);
+        }
+
+        i = currentEntry[2];
+        j = currentEntry[3];
+    }
+
+    return sequence;
+}
---
+++ c#/algoExpert/MinNumberOfJumps/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Min Number of Jumps
+ *
+ * You're given a non-empty array of positive integers where each integer represents the
+ * maximum number of steps you can take forward in the array. For example, if the
+ * element at index 1 is 3, you can go from index 1 to index 2, 3, or 4.
+ *
+ * Write a function that returns the minimum number of jumps needed to reach the final
+ * index.
+ *
+ * Note that jumping from index i to index i + x always constitutes one jump, no
+ * matter how large x is.
+ */
+
+var array = new int[]{ 3, 4, 2, 1, 2, 3, 7, 1, 1, 1, 3 };
+var result = MinNumberOfJumpsV1(array);
+result = MinNumberOfJumpsV2(array);
+
+static int MinNumberOfJumpsV1(int[] array)
+{
+    int[] jumps = new int[array.Length];
+    Array.Fill(jumps, Int32.MaxValue);
+    jumps[0] = 0;
+    for (int i = 1; i < array.Length; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (array[j] + j >= i)
+            {
+                jumps[i] = Math.Min(jumps[j] + 1, jumps[i]);
+            }
+        }
+    }
+
+    return jumps[jumps.Length - 1];
+}
+
+static int MinNumberOfJumpsV2(int[] array)
+{
+    if (array.Length == 1) return 0;
+
+    int jumps = 0;
+    int maxReach = array[0];
+    int steps = array[0];
+
+    for (int i = 1; i < array.Length - 1; i++)
+    {
+        maxReach = Math.Max(maxReach, i + array[i]);
+        steps -= 1;
+        if (steps == 0)
+        {
+            jumps += 1;
+            steps = maxReach - i;
+        }
+    }
+    
+    return jumps + 1; 
+}
